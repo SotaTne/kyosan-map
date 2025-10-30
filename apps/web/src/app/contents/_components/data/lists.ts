73bffce0846664d33996736af5280d1366ea461a
TSUJI-Shogo <scorpii.jabbah@gmail.com>
2025-10-30 19:48:34 +0900

    仮のコンテンツデータ更新とモデル・音楽ファイル追加

--- apps/web/src/app/contents/_components/data/lists.ts
+++ apps/web/src/app/contents/_components/data/lists.ts
@@ -1,21 +1,23 @@
 // ======== データ定義 ========
 
 // A: アイコン画像
-export const listImage: string[] = ["/image1.png", "/image2.png"];
+export const listImage: string[] = ["/images/image-ico1.png", "/images/image-ico2.png","/images/image-ico3.png", "/images/image-ico4.png"];
 
 // B: 音楽
 export const listMusic: { icon: string; song: string; title: string }[] = [
-  { icon: "/image3.png", song: "/song1.mp3", title: "星屑の旋律" },
-  { icon: "/image4.png", song: "/song2.mp3", title: "夜空の旅路" },
+  { icon: "/images/image-ico1.png", song: "/musics/music1.mp3", title: "星屑の旋律" },
+  { icon: "/images/image-ico2.png", song: "/musics/music2.mp3", title: "夜空の旅路" },
 ];
 
 // C: 3Dモデル
 export const listModel: { icon: string; model: string; title: string }[] = [
-  { icon: "/dummy.png", model: "/model1.glb", title: "宇宙船モデル" },
+  { icon: "/images/image-ico3.png", model: "/models/model1.glb", title: "宇宙船モデル" },
 ];
 
 // Aリストのアイコン → 本命画像マッピング
 export const imageMapping: Record<string, string> = {
-  "/image1.png": "/image-collection1.png",
-  "/image2.png": "/image-collection2.png",
+  "/images/image-ico1.png": "/images/image-data1.png",
+  "/images/image-ico2.png": "/images/image-data2.png",
+  "/images/image-ico3.png": "/images/image-data3.png",
+  "/images/image-ico4.png": "/images/image-data4.png",
 };
